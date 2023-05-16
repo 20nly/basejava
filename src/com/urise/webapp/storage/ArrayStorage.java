17c22be84a608cb4a86be71f59a6c28ff1a8837a
Grand <fgt0191@gmail.com>
2023-05-16 23:56:52 +0500

    Update ArrayStorage

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,52 @@
+package com.urise.webapp.storage;
+import com.urise.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+    }
+    public void update(Resume r) {
+        //TODO check if resume is present
+        System.out.println("ERROR");
+    }
+
+    public void save(Resume r) {
+        //TODO check if resume not present
+    }
+
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()){
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        //TODO check if resume is present
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()){
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
+                size--;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    public int size() {
+        return 0;
+    }
+}
